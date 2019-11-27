b86934686115ae866285bc542db69818e0a7f2bf
julybro <baro.julia@gmail.com>
2019-11-27 21:15:31 +0100

    build dir

---
+++ build/precache-manifest.c0453c9b2a5267865a759888155c83e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece6077f0b3cedec798aa9d518cd888c",
+    "url": "/shopping-list-app/index.html"
+  },
+  {
+    "revision": "cd7533dc7bd46a13090f",
+    "url": "/shopping-list-app/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "3529245ab985caf43003",
+    "url": "/shopping-list-app/static/css/main.2096efc3.chunk.css"
+  },
+  {
+    "revision": "cd7533dc7bd46a13090f",
+    "url": "/shopping-list-app/static/js/2.1c6a668f.chunk.js"
+  },
+  {
+    "revision": "3529245ab985caf43003",
+    "url": "/shopping-list-app/static/js/main.99f8c7d2.chunk.js"
+  },
+  {
+    "revision": "56b0b4842d192ddb14a6",
+    "url": "/shopping-list-app/static/js/runtime-main.fbc0f590.js"
+  },
+  {
+    "revision": "80dd12ab1585f26c6def1ece26979274",
+    "url": "/shopping-list-app/static/media/bg.80dd12ab.png"
+  }
+]);
